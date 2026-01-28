2b61a4147bc7c1cf112f93742c64dd233ebe8f88
Vishal Karnakoti <vishalkarnakotinagekar@gmail.com>
2026-01-29 00:08:07 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const alertRoutes = require("./routes/alertRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/womensafety")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+app.use("/api/alerts", alertRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
